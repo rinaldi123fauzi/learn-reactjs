a2d7ec4a9292fea38a4e263203d87808b9d16d4f
rinalfauzi <rinalfauzi@gmail.com>
2020-10-27 21:35:49 +0700

    Index as Key

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -22,9 +22,11 @@ function NameList() {
             skill: 'Ruby'
         }
     ]
-    const names = ['Diana','Robby','Tono']
-    const nameList = names.map(name => <h3>{name}</h3>)
-    const personList = persons.map(person => <Person person={person}></Person>)
+    // const names = ['Diana','Robby','Tono']
+    // const nameList = names.map(name => <h3>{name}</h3>)
+    const personils = ['Ucup','Ujang','Eko']
+    const nameList = personils.map((personil, index) => <h1 key={index}>{index += 1}. {personil}</h1>)
+    const personList = persons.map(person => <Person key={person.id} person={person}></Person>)
     return (
         <div>
             {nameList}
